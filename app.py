b7ec16c18060238df04206f024609864f8b7d6ab
Cablethief <mikek3262@gmail.com>
2021-05-31 11:32:23 +0200

    add route catch all

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request
 import sys
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    print(str(request.headers.get("Azurejwt")) + "|" + str(request.headers.get("User-Agent")))
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def all_routes(path):
+    print(str(path) + "|" + str(request.headers.get("Azurejwt")) + "|" + str(request.headers.get("User-Agent")))
     return render_template('index.html')
 
