bb94dceb42419021a08e5ca265c4661238b87382
Cablethief <mikek3262@gmail.com>
2021-05-31 08:49:01 +0200

    quick token printer

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sys
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    print(str(request.headers.get("Azurejwt")) + "|" + str(request.headers.get("User-Agent")))
+    return render_template('index.html')
+
